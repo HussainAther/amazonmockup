8047eccf1634b5849d54b69dca937018fa7ad4df
Syed Hussain Ather <shussainather@gmail.com>
2023-05-23 17:26:38 -0400

    comments redone

--- src/script.js
+++ src/script.js
@@ -6,18 +6,20 @@ document.getElementById('signInForm').addEventListener('submit', function(event)
 
   // Perform sign-in validation
   if (email === 'example@example.com' && password === 'password') {
-    alert('Sign in successful!');
-    // Redirect to another page or perform further actions
+    alert('Signed in.');
+    // Either initiate a redirect to a different page or proceed with additional actions.
   } else {
-    alert('Invalid email or password. Please try again.');
+    alert('Unable to sign in.');
     // Clear the form or display error messages
   }
 });
 
 document.querySelector('.create-account-button').addEventListener('click', function() {
-  alert('Create your Amazon account!');
-  // Redirect to account creation page or perform further actions
+  alert('Make your account!');
+  // Either redirect to the account creation page or proceed with additional actions.
 });
 
 document.querySelector('.help-link').addEventListener('click', function() {
-  alert('Need Help
+  alert('Need Help');
+});
+
